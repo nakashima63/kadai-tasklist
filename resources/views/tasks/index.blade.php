e3fc604783ef84728c4a90f0989393ce53e183fd
soya.nakashima <so8.63.nakaso@gmail.com>
2021-07-06 06:43:31 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -9,7 +9,8 @@
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>タスク</th></th>
+                    <th>ステータス</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,6 +19,7 @@
                     {{-- タスク詳細ページへのリンク --}}
                     
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->status }}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
                 @endforeach
